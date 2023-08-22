ebd627c0c9500c2ef52a2b1b356e0aa434996694
andreza oliveira <118784915+andrezarsoliveira@users.noreply.github.com>
2023-08-22 19:29:24 -0300

    Update exercicio27a.py

--- exercicio27a.py
+++ exercicio27a.py
@@ -1,12 +1,12 @@
 #EXERCÍCIO 27
-morango = float(input("Digite a quantidade de morangos em kg: "))
-maca = float(input("Digite a quantidade de maças em kg: "))
+morango_kg = float(input("Digite a quantidade de morangos em kg: "))
+maca_kg = float(input("Digite a quantidade de maças em kg: "))
 #MORANGOS
-preco_menor_mor = morango * 2.50
-preco_maior_mor = morango * 2.20
+preco_menor_mor = morango_kg * 2.50
+preco_maior_mor = morango_kg * 2.20
 #MAÇÃ
-preco_menor_mac = maca * 1.80
-preco_maior_mac = maca * 1.50
+preco_menor_mac = maca_kg * 1.80
+preco_maior_mac = maca_kg * 1.50
 #CÁLCULO DO MORANGO
 if morango >= 5:
     preco_total_mor = preco_menor_mor
@@ -19,7 +19,7 @@ else:
     preco_total_mac = preco_maior_mac
 preco_total_das_frutas = preco_total_mor + preco_total_mac
 #CÁLCULO TOTAL DAS FRUTAS
-if preco_total_das_frutas >= 25 or (morango + maca) >= 8:
+if preco_total_das_frutas >= 25 or (morango_kg + maca_kg) >= 8:
     print("Valor final: ", (preco_total_mor - (preco_total_mac * 0.1)))
 else:
     print("Valor final: ", preco_total_das_frutas)
