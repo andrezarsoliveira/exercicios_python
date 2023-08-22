b4e19addecc7b49006b23f0e559e037702b54b1f
andreza oliveira <118784915+andrezarsoliveira@users.noreply.github.com>
2023-08-22 18:42:06 -0300

    Add files via upload

---
+++ exercicio27a.py
@@ -0,0 +1,28 @@
+#EXERCÍCIO 27
+morango = float(input("Digite a quantidade de morangos em kg: "))
+maca = float(input("Digite a quantidade de maças em kg: "))
+#MORANGOS
+preco_menor_mor = morango * 2.50
+preco_maior_mor = morango * 2.20
+#MAÇÃ
+preco_menor_mac = maca * 1.80
+preco_maior_mac = maca * 1.50
+#CÁLCULO DO MORANGO
+if morango >= 5:
+    preco_total_mor = preco_menor_mor
+else:
+    preco_total_mor = preco_maior_mor
+#CÁLCULO DA MAÇÃ
+if maca >= 5:
+    preco_total_mac = preco_menor_mac
+else:
+    preco_total_mac = preco_maior_mac
+preco_total_das_frutas = preco_total_mor + preco_total_mac
+#CÁLCULO TOTAL DAS FRUTAS
+if preco_total_das_frutas >= 25 or (morango + maca) >= 8:
+    print("Valor final: ", (preco_total_mor - (preco_total_mac * 0.1)))
+else:
+    print("Valor final: ", preco_total_das_frutas)
+
+
+
